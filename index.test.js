cb1c51d823e962d4c132b39f5cad9d538461c4a8
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-12-30 15:25:46 -0300

    fix script assert test

--- index.test.js
+++ index.test.js
@@ -19,15 +19,6 @@ function create_canva () {
 	return root;
 }
 
-function import_script () {
-	let script = document.createElement("script");
-	script.type = "text/javascript";
-	script.src = "main.umd";
-	script.setAttribute("boot", true);
-	document.body.appendChild(script);
-	return script;
-}
-
 test("General test", async t => {
 	const browser = await puppeteer.launch(conf);
 	const page = await browser.newPage();
@@ -35,11 +26,27 @@ test("General test", async t => {
 		throw err;
 	});
 	await page.evaluate(create_canva);
-	await page.evaluate(import_script);
-	await t.test("App initializated", async t => {
-		await page.evaluate(() => {
-			document.getElementById("hp-scada");
+	await t.test("Script Loaded", async t => {
+		let script = await page.evaluate(async () => {
+			let script = document.createElement("script");
+			script.type = "text/javascript";
+			script.src = "file:///index.js";
+			script.setAttribute("boot", true);
+			script.onerror = () => { throw new Error() };
+			document.body.appendChild(script);
+			let promise = new Promise((resolve, reject) => {
+				script.onerror = reject(false);
+				script.onload = resolve(true);
+			});
+			let result = await promise;
 		});
+		await assert.ok();
+	});
+	await t.test("App initializated", async t => {
+		let app = await page.evaluate(() => {
+			return document.getElementById("hp-scada");
+		});
+		assert.notEqual(app, null);
 	});
 	await browser.close();
 });
