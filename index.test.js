4474382954adf08ed0beb6f14ca43b6c095434b1
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-12-30 14:42:14 -0300

    make tags

--- index.test.js
+++ index.test.js
@@ -12,11 +12,26 @@ const conf = {
 	executablePath: "/usr/bin/google-chrome"
 };
 
+function create_canva () {
+	let root = document.createElement("div");
+	root.id = "viewContent";
+	document.body.appendChild(root);
+	return root;
+}
+
+function import_script () {
+	let script = document.createElement("script");
+	script.type = "text/javascript";
+	script.src = "main.umd";
+	document.body.appendChild(script);
+	return script;
+}
+
 test("General test", async t => {
 	const browser = await puppeteer.launch(conf);
 	const page = await browser.newPage();
-	let response = page.evaluate(word => `you said ${word}`, "Xuxu");
-	assert.equal(response, "xuxu");
+	await page.evaluate(create_canva);
+	await page.evaluate(import_script);
 	await browser.close();
 });
 
