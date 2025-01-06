432a914f74cc0775044cd58c45e6f4b501f0e4f3
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2025-01-06 16:00:23 -0300

    make error message

--- src/prototype/StatefulComponent.js
+++ src/prototype/StatefulComponent.js
@@ -7,11 +7,15 @@ class StatefulComponent extends Component {
 		let nodeTag = 'td';
 		super (map, nodeTag, className);
 		this.datapoint = datapoint;
+		this.errorCount = 0;
 	}
 
 	fetch_value () {
 		DatapointApi.tag_load_num(this.datapoint).then(value => {
 			this.update_value(value);
+		}).catch((error) => {
+			this.errorCount += 1;
+			console.error({ error, Component: this });
 		});
 	}
 
@@ -21,6 +25,24 @@ class StatefulComponent extends Component {
 
 	update_value (value) {
 		this.node.innerText = value;
+	}
+
+	write_error () {
+		update_value("\u274C");
+	}
+
+	start_loop () {
+		this.loopPointer = setInterval(() => {
+			if (this.errorCount > 3) {
+				this.fetch_value();
+			} else {
+				this.stop_loop();
+			}
+		}, 5e3);
+	}
+
+	stop_loop () {
+		clearInterval(this.loopPointer);
 	}
 }
 
